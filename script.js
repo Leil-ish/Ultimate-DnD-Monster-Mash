af86d89bbe8e24d64274115fae34bc48075835eb
Leila Anderson <leilacanderson@gmail.com>
2019-05-04 18:28:13 -0500

    instruction clarifications

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ function displayResults(responseJson, battleSize) {
   for (let i = 0; i < responseJson.results.length & i < battleSize; i++) {
         $('#results-list').append(
             `<ul>
-                <li><h3>Name: ${responseJson.results[i].name}</h3></li>
-                <li><p>Type: ${responseJson.results[i].type}</p></li>
-                <li><p>Size: ${responseJson.results[i].size}</p></li>
-                <li><p>Armor Class: ${responseJson.results[i].armor_class}</p></li>
-                <li><p>Hit Points: ${responseJson.results[i].hit_points}</p></li>
-                <li><p>Damage Vulnerabilities: ${responseJson.results[i].damage_vulnerabilities}</p></li>
-                <li><p>Damage Resistances: ${responseJson.results[i].damage_resistances}</p></li>
-                <li><p>Damage Immunities: ${responseJson.results[i].damage_immunities}</p></li>
-                <li><p>Condition Immunities: ${responseJson.results[i].condition_immunities}</p></li>
-                <li><p>Actions: ${responseJson.results[i].actions.name}</p></li>`
+                    <li><h3>Name: ${responseJson.results[i].name}</h3></li>
+                    <li><p>Type: ${responseJson.results[i].type}</p></li>
+                    <li><p>Size: ${responseJson.results[i].size}</p></li>
+                    <li><p>Armor Class: ${responseJson.results[i].armor_class}</p></li>
+                    <li><p>Hit Points: ${responseJson.results[i].hit_points}</p></li>
+                    <li><p>Damage Vulnerabilities: ${responseJson.results[i].damage_vulnerabilities}</p></li>
+                    <li><p>Damage Resistances: ${responseJson.results[i].damage_resistances}</p></li>
+                    <li><p>Damage Immunities: ${responseJson.results[i].damage_immunities}</p></li>
+                    <li><p>Condition Immunities: ${responseJson.results[i].condition_immunities}</p></li>
+                </ul>`
     )}
   $('#results').removeClass('hidden');
 };
