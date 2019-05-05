9d39a8c7263809a3a7955f726c4bc9eda410ab14
Leila Anderson <leilacanderson@gmail.com>
2019-05-05 16:24:22 -0500

    added collapsible for results

--- script.js
+++ script.js
@@ -7,19 +7,35 @@ function displayResults(responseJson, battleSize) {
   $('#results-list').empty();
   for (let i = 0; i < responseJson.results.length & i < battleSize; i++) {
         $('#results-list').append(
-            `<ul>
-                    <li><h3>Name: ${responseJson.results[i].name}</h3></li>
-                    <li><p>Type: ${responseJson.results[i].type}</p></li>
-                    <li><p>Size: ${responseJson.results[i].size}</p></li>
-                    <li><p>Armor Class: ${responseJson.results[i].armor_class}</p></li>
-                    <li><p>Hit Points: ${responseJson.results[i].hit_points}</p></li>
-                    <li><p>Damage Vulnerabilities: ${responseJson.results[i].damage_vulnerabilities}</p></li>
-                    <li><p>Damage Resistances: ${responseJson.results[i].damage_resistances}</p></li>
-                    <li><p>Damage Immunities: ${responseJson.results[i].damage_immunities}</p></li>
-                    <li><p>Condition Immunities: ${responseJson.results[i].condition_immunities}</p></li>
-                </ul>`
+            `<button class="collapsible">${responseJson.results[i].name}</button>
+                <div class="content">
+                    <ul>
+                        <li><p>Type: ${responseJson.results[i].type}</p></li>
+                        <li><p>Size: ${responseJson.results[i].size}</p></li>
+                        <li><p>Armor Class: ${responseJson.results[i].armor_class}</p></li>
+                        <li><p>Hit Points: ${responseJson.results[i].hit_points}</p></li>
+                        <li><p>Damage Vulnerabilities: ${responseJson.results[i].damage_vulnerabilities}</p></li>
+                        <li><p>Damage Resistances: ${responseJson.results[i].damage_resistances}</p></li>
+                        <li><p>Damage Immunities: ${responseJson.results[i].damage_immunities}</p></li>
+                        <li><p>Condition Immunities: ${responseJson.results[i].condition_immunities}</p></li>
+                    </ul>
+                </div>`
     )}
   $('#results').removeClass('hidden');
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+      content.style.display = "none";
+      } else {
+      content.style.display = "block";
+      }
+  });
+  }
 };
 
 function rollForMonsters(battleSize, challengeRating) {
@@ -41,6 +57,7 @@ function rollForMonsters(battleSize, challengeRating) {
         $('#js-error-message').text(`Critical fail! ${err.message}`);
     });
 }
+
 
 function watchForm() {
   $('form').submit(event => {
