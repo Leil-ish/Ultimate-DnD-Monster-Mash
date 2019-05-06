aa5095d205eca0099fe5eb4d08cc0f57e228155e
Leila Anderson <leilacanderson@gmail.com>
2019-05-06 15:04:06 -0500

    styling added

--- script.js
+++ script.js
@@ -10,15 +10,19 @@ function displayResults(responseJson, battleSize) {
             `<button class="collapsible">&#10133; ${responseJson.results[i].name}</button>
                 <div class="content">
                     <ul>
-                        <li><p><a href = "https://beta.open5e.com/monsters/${responseJson.results[i].slug}/"  target="_blank">Full Stats</a></p></li>
-                        <li><p>Type: ${responseJson.results[i].type}</p></li>
-                        <li><p>Size: ${responseJson.results[i].size}</p></li>
-                        <li><p>Armor Class: ${responseJson.results[i].armor_class}</p></li>
-                        <li><p>Hit Points: ${responseJson.results[i].hit_points}</p></li>
-                        <li><p>Damage Vulnerabilities: ${responseJson.results[i].damage_vulnerabilities}</p></li>
-                        <li><p>Damage Resistances: ${responseJson.results[i].damage_resistances}</p></li>
-                        <li><p>Damage Immunities: ${responseJson.results[i].damage_immunities}</p></li>
-                        <li><p>Condition Immunities: ${responseJson.results[i].condition_immunities}</p></li>
+                        <li><h3>Basic info:</h3></li>
+                            <ul>
+                                <li><p>Type: ${responseJson.results[i].type}</p></li>
+                                <li><p>Size: ${responseJson.results[i].size}</p></li>
+                                <li><p>Armor Class: ${responseJson.results[i].armor_class}</p></li>
+                                <li><p>Hit Points: ${responseJson.results[i].hit_points}</p></li>
+                                <li><p>Damage Vulnerabilities: ${responseJson.results[i].damage_vulnerabilities}</p></li>
+                                <li><p>Damage Resistances: ${responseJson.results[i].damage_resistances}</p></li>
+                                <li><p>Damage Immunities: ${responseJson.results[i].damage_immunities}</p></li>
+                                <li><p>Condition Immunities: ${responseJson.results[i].condition_immunities}</p></li>
+                            </ul><br>
+                        <li><p><a href = "https://beta.open5e.com/monsters/${responseJson.results[i].slug}/" 
+                        target="_blank" id="statsLink">Full Stats & Actions</a></p></li>
                     </ul>
                 </div>`
     )}
