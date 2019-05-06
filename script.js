d01da93e8fcd6a9c2644dc08948854f0cf52f7c7
Leila Anderson <leilacanderson@gmail.com>
2019-05-06 17:21:39 -0500

    style changes on collapsible

--- script.js
+++ script.js
@@ -10,17 +10,14 @@ function displayResults(responseJson, battleSize) {
             `<button class="collapsible">&#10133; ${responseJson.results[i].name}</button>
                 <div class="content">
                     <ul>
-                        <li><h3>Basic info:</h3></li>
-                            <ul>
-                                <li><p>Type: ${responseJson.results[i].type}</p></li>
-                                <li><p>Size: ${responseJson.results[i].size}</p></li>
-                                <li><p>Armor Class: ${responseJson.results[i].armor_class}</p></li>
-                                <li><p>Hit Points: ${responseJson.results[i].hit_points}</p></li>
-                                <li><p>Damage Vulnerabilities: ${responseJson.results[i].damage_vulnerabilities}</p></li>
-                                <li><p>Damage Resistances: ${responseJson.results[i].damage_resistances}</p></li>
-                                <li><p>Damage Immunities: ${responseJson.results[i].damage_immunities}</p></li>
-                                <li><p>Condition Immunities: ${responseJson.results[i].condition_immunities}</p></li>
-                            </ul><br>
+                        <li><p>Type: ${responseJson.results[i].type}</p></li>
+                        <li><p>Size: ${responseJson.results[i].size}</p></li>
+                        <li><p>Armor Class: ${responseJson.results[i].armor_class}</p></li>
+                        <li><p>Hit Points: ${responseJson.results[i].hit_points}</p></li>
+                        <li><p>Damage Vulnerabilities: ${responseJson.results[i].damage_vulnerabilities}</p></li>
+                        <li><p>Damage Resistances: ${responseJson.results[i].damage_resistances}</p></li>
+                        <li><p>Damage Immunities: ${responseJson.results[i].damage_immunities}</p></li>
+                        <li><p>Condition Immunities: ${responseJson.results[i].condition_immunities}</p></li><br>
                         <li><p><a href = "https://beta.open5e.com/monsters/${responseJson.results[i].slug}/" 
                         target="_blank" id="statsLink">Full Stats & Actions</a></p></li>
                     </ul>
@@ -70,7 +67,7 @@ function validateChallengeRating() {
     if (x < 0 || x > 30) {
         $('#results').empty();
         $('#js-main').html(
-        `Challenge rating needs to be 0, 1/8, 1/4, 1/2, or an integer from 1 to 30.<br>
+        `<h2>Challenge rating needs to be 0, 1/8, 1/4, 1/2, or an integer from 1 to 30.</h2><br>
         <form>
         <input type="submit" class="button" value="Roll again?"></input>
         </form>`
@@ -89,7 +86,7 @@ function validateBattleSize() {
     if (isNaN(y) || y < 2 || y > 10) {
         $('#results').empty();
         $('#js-main').html(
-            `Battle size needs to be an integer from 2 to 10.<br>
+            `<h2>Battle size needs to be an integer from 2 to 10.</h2><br>
             <form>
             <input type="submit" class="button" value="Roll again?"></input>
             </form>`
